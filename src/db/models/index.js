7eea4968dd0cf42b1be9b644774b80dd0743eecd
bleek42 <brandonleek42@gmail.com>
2023-05-09 12:00:21 -0400

    changing bootstrapModels to use less loops

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -19,28 +19,31 @@ async function bootstrapModels() {
   try {
     const db = await initDb();
 
-    await fs
-      .readdir(__dirname)
-      .filter((file) => {
-        console.log('files:', file);
-        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-      })
-      .forEach((file) => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-      });
-
-    Object.keys(db).forEach((modelName) => {
-      if (db[modelName].associate) {
-        db[modelName].associate(db);
-      }
+    const models = await fs.readdir(__dirname).filter((file) => {
+      console.log('files:', file);
+      return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
     });
+
+    for await (const [key, value] of Object.entries(models)) {
+      console.log(db);
+      console.table({ key, value });
+    }
+    // .forEach((file) => {
+    //   const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    //   db[model.name] = model;
+    // });
+
+    // Object.keys(db).forEach((modelName) => {
+    //   if (db[modelName].associate) {
+    //     db[modelName].associate(db);
+    //   }
+    // });
+    return { db, Sequelize };
   } catch (err) {
     console.error(err);
     throw err;
   }
 
-  return { db, Sequelize };
 }
 
 module.exports = bootstrapModels;
